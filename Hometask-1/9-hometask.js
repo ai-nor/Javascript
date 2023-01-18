a2c0aa01bba981a4ea73ce31ccd5b675c6250f2b
Tetyana Norynchak <tnorinchak@admixer.ua>
2023-01-18 21:46:17 +0000

    add 8 and 9 tasks

--- Hometask-1/9-hometask.js
+++ Hometask-1/9-hometask.js
@@ -6,3 +6,83 @@
 
     Підказка(https://ukr-lifehacks.ed-era.com/rozdil-9/zvyazok_chislivnykiv)
     */
+
+    //1 - пусто
+    //2,3,4 - и
+    //5,6,7.8.9.11, 12, 13, 14, 15, 16, 17, 18, 19, 0 - ів
+    //дрібне - а
+
+
+let metr =2563.25;
+let km = metr/1000;
+
+let namem = "метр";
+let namekm = "кілометр";
+
+let convertm = metr.toString();
+let convertkm = km.toString();
+
+let lastm = convertm[convertm.length-1]; 
+console.log(lastm);
+
+
+let lastkm = metr.toString();
+
+
+if (!Number.isInteger(metr)) {
+    endm = "а";
+}
+else if (convertm.length ==1 && lastm == 1) {
+    endm = "";
+}
+else if ((convertm.length ===1) && (lastm == 2 || lastm == 3 || lastm == 4 )) {
+    endm = "и";
+}
+else if ((convertm.length ===1) && (lastm == 5 || lastm == 6 || lastm == 7 || lastm == 8 || lastm == 9)) {
+    endm = "ів";
+}
+else if (convertm.length ===2 && convertm[convertm.length-2] == 1) {
+    endm = "ів";
+}
+else if (convertm.length >=2 && lastm == 1) {
+    endm = "";
+}
+else if ((convertm.length >=2) && (lastm == 2 || lastm == 3 || lastm == 4 )) {
+    endm = "и";
+}
+else if ((convertm.length >=2) && (lastm == 5 || lastm == 6 || lastm == 7 || lastm == 8 || lastm == 9)) {
+    endm = "ів";
+}
+else {
+    console.log ("Щось не врахувала");
+}
+
+if (!Number.isInteger(km)) {
+    endkm = "а";
+}
+else if (convertkm.length ==1 && lastm == 1) {
+    endkm = "";
+}
+else if ((convertkm.length ===1) && (lastm == 2 || lastm == 3 || lastm == 4 )) {
+    endkm = "и";
+}
+else if ((convertkm.length ===1) && (lastm == 5 || lastm == 6 || lastm == 7 || lastm == 8 || lastm == 9)) {
+    endkm = "ів";
+}
+else if (convertkm.length ===2 && convertm[convertm.length-2] == 1) {
+    endkm = "ів";
+}
+else if (convertkm.length >=2 && lastm == 1) {
+    endkm = "";
+}
+else if ((convertkm.length >=2) && (lastm == 2 || lastm == 3 || lastm == 4 )) {
+    endkm = "и";
+}
+else if ((convertkm.length >=2) && (lastm == 5 || lastm == 6 || lastm == 7 || lastm == 8 || lastm == 9)) {
+    endkm = "ів";
+}
+else {
+    console.log ("Щось не врахувала");
+}
+
+console.log (metr + " " + namem + endm + " це " + km + " " + namekm + endkm);
